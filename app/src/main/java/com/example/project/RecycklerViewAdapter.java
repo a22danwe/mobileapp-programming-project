df222e62a6bf31f3552c1642bc186cb81a089c0e
a22danwe <a22danwe@student.his.se>
2023-05-19 15:36:44 +0200

    skapat en klass för adapter och lagt in koden för den

---
+++ app/src/main/java/com/example/project/RecycklerViewAdapter.java
@@ -0,0 +1,52 @@
+package com.example.project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class RecyclerViewAdapter  extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private List<Mountain> mountain;
+    private LayoutInflater layoutInflater;
+
+    RecyclerViewAdapter(Context context,List<Mountain> mountain) {
+        this.mountain = mountain;
+        this.layoutInflater=LayoutInflater.from(context);
+    }
+
+    @Override
+    @NonNull
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.textview, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.title.setText(mountain.get(position).toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mountain.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.title);
+        }
+
+    }
+}
