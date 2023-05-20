a9cf6e91c5e0d89ffe298018da6526082b630527
a22danwe <a22danwe@student.his.se>
2023-05-20 14:06:49 +0200

    lagt textview i mainactivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,21 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.EventLogTags;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 class MainActivity extends AppCompatActivity {
-    TextView Beskrivning;
+    TextView Description;
     Button pressButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pressButton = findViewById(R.id.pressButton);
-        //TextView = findViewById(R.id.Beskrivning);
+        Description = findViewById(R.id.Description);
 
         pressButton.setOnClickListener(new View.OnClickListener() {
             @Override
